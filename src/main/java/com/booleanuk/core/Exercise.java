cc2aa883a8982f63a90417ad74123b77d1117944
Piotr Karolak <karolak.piotr.012@gmail.com>
2023-07-06 10:16:45 +0200

    exercise 1

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -4,4 +4,24 @@ public class Exercise {
     public static String sayHelloWorld() {
         return "Hello, world!";
     }
+
+
+    public static void main(String[] args) {
+        loop(12);
+        loopTwo(12,12);
+
+    }
+
+
+    public static void loop(int number) {
+        for (int i = 1; i <= 12; i++){
+            System.out.println(number*i);
+        }
+    }
+
+    public static void loopTwo(int number, int multiply) {
+        for (int i = 1; i <= multiply; i++){
+            System.out.println(number*i);
+        }
+    }
 }
