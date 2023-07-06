81c3c6d505d062b750b9976a4e5f1993d3e04afa
Piotr Karolak <karolak.piotr.012@gmail.com>
2023-07-06 10:17:14 +0200

    exercise 1

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -7,19 +7,19 @@ public class Exercise {
 
 
     public static void main(String[] args) {
-        loop(12);
-        loopTwo(12,12);
+        activityOne(12);
+        activityTwo(12,12);
 
     }
 
 
-    public static void loop(int number) {
+    public static void activityOne(int number) {
         for (int i = 1; i <= 12; i++){
             System.out.println(number*i);
         }
     }
 
-    public static void loopTwo(int number, int multiply) {
+    public static void activityTwo(int number, int multiply) {
         for (int i = 1; i <= multiply; i++){
             System.out.println(number*i);
         }
